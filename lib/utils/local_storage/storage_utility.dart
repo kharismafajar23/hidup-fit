a0af7abc05bc82f25d2838f4621e9fe2934d48da
Kharisma Fajar <restuistyo@gmail.com>
2025-05-27 00:47:33 +0700

    ubah dikit

--- lib/utils/local_storage/storage_utility.dart
+++ lib/utils/local_storage/storage_utility.dart
@@ -1,24 +1,24 @@
 import 'package:get_storage/get_storage.dart';
 
-class TLocalStorage {
-  static final TLocalStorage _instance = TLocalStorage._internal();
+class UseLocalStorage {
+  static final UseLocalStorage _instance = UseLocalStorage._internal();
 
-  factory TLocalStorage() {
+  factory UseLocalStorage() {
     return _instance;
   }
 
-  TLocalStorage._internal();
+  UseLocalStorage._internal();
 
   final _storage = GetStorage();
 
   // Generic method to save data
-  Future<void> saveData<T>(String key, T value) async {
+  Future<void> saveData<K>(String key, K value) async {
     await _storage.write(key, value);
   }
 
   // Generic method to read data
-  T? readData<T>(String key) {
-    return _storage.read<T>(key);
+  K? readData<K>(String key) {
+    return _storage.read<K>(key);
   }
 
   // Generic method to remove data
