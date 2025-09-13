4fec0142f7fc93c5ca70350aeeb23ff56a14b832
Kharisma Fajar <restuistyo@gmail.com>
2025-09-13 11:15:25 +0700

    Tambah Package OpenAi dan ubah git ignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:hidup_fit/routes/app_pages.dart';
 import 'package:hidup_fit/routes/app_routes.dart';
 import 'package:hidup_fit/utils/theme/theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await GetStorage.init();
+  await dotenv.load(fileName: ".env");
+  OpenAI.apiKey = dotenv.env['OPENAI_API_KEY'] ?? "";
   runApp(App());
 }
 
